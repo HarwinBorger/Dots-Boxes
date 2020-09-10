4a0aa6937c8e4e438a561cf1986edcdcc0ce1563
Harwin Borger <harwin@studioborger.nl>
2020-09-10 13:03:55 +0200

    deploy

---
+++ precache-manifest.dc660b38436ace043f41be9ccf8effb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6756a84ed28462356f6bb69162a3ec39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d564ed2530950064b21",
+    "url": "/static/css/main.f07450c9.chunk.css"
+  },
+  {
+    "revision": "9b720ecfa0dc56462e93",
+    "url": "/static/js/2.cc154144.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.cc154144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d564ed2530950064b21",
+    "url": "/static/js/main.cf88a4bf.chunk.js"
+  },
+  {
+    "revision": "f66e0d5be09d402c660f",
+    "url": "/static/js/runtime-main.da5b834c.js"
+  }
+]);
